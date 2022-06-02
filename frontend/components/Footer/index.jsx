0f826ac65d0eaf24d5c47c07b6dc8d2afcbb3c87
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-06-02 13:22:50 +0530

    team cards ui improved and footer icons

--- frontend/components/Footer/index.jsx
+++ frontend/components/Footer/index.jsx
@@ -11,7 +11,7 @@ function Footer() {
               ISTE STUDENTS' CHAPTER <br></br> NIT DURGAPUR
             </h4>
             <ul className={s.ul1}>
-              <li>Made with 🤍 by WEBD Team</li>
+              <li>Made with 💖 by WEBD Team</li>
               <li>© 2022 ISTE. All Rights Reserved. </li>
             </ul>
           </div>
@@ -22,7 +22,7 @@ function Footer() {
                 <a href="#">location</a>
               </li>
               <li>
-                <a href="#">phone n umber</a>
+                <a href="#">phone number</a>
               </li>
               <li>
                 <a href="#">mail</a>
@@ -49,36 +49,48 @@ function Footer() {
           <div className={s.footerCol}>
             <h4>follow us</h4>
             <div className="flex space-x-4">
-              <a href="#" className="flex items-center justify-center">
+              <a
+                href="#"
+                className="p-2 rounded-full bg-gray-500 transition-all duration-300 hover:bg-white group"
+              >
                 <svg
-                  className="w-6 h-6 text-yellow-500 hover:text-yellow-400 fill-current transition-all duration-300 hover:-translate-y-1 "
+                  className="w-5 h-5 text-white group-hover:text-yellow-500 fill-current  "
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 512 512"
                 >
                   <path d="M464 64C490.5 64 512 85.49 512 112C512 127.1 504.9 141.3 492.8 150.4L275.2 313.6C263.8 322.1 248.2 322.1 236.8 313.6L19.2 150.4C7.113 141.3 0 127.1 0 112C0 85.49 21.49 64 48 64H464zM217.6 339.2C240.4 356.3 271.6 356.3 294.4 339.2L512 176V384C512 419.3 483.3 448 448 448H64C28.65 448 0 419.3 0 384V176L217.6 339.2z" />
                 </svg>
               </a>
-              <a href="#">
+              <a
+                href="#"
+                className="p-2 rounded-full bg-gray-500 transition-all duration-300 hover:bg-white group"
+              >
                 <svg
-                  className="w-6 h-6 text-blue-600 hover:text-blue-400 fill-current transition-all duration-300 hover:-translate-y-1 "
+                  className="w-5 h-5 text-white group-hover:text-blue-500 fill-current  "
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 24 24"
                 >
                   <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z" />
                 </svg>
               </a>
-              <a href="#">
+              <a
+                href="#"
+                className="p-2 rounded-full bg-gray-500 transition-all duration-300 hover:bg-white group"
+              >
                 <svg
-                  className="w-6 h-6 text-red-600 hover:text-red-400 fill-current transition-all duration-300 hover:-translate-y-1 "
+                  className="w-5 h-5 text-white group-hover:text-red-500 fill-current  "
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 16 16"
                 >
                   <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
                 </svg>
               </a>
-              <a href="#">
+              <a
+                href="#"
+                className="p-2 rounded-full bg-gray-500 transition-all duration-300 hover:bg-white group"
+              >
                 <svg
-                  className="w-6 h-6 text-indigo-600 hover:text-indigo-400 fill-current transition-all duration-300 hover:-translate-y-1 "
+                  className="w-5 h-5 text-white group-hover:text-indigo-500 fill-current  "
                   xmlns="http://www.w3.org/2000/svg"
                   viewBox="0 0 448 512"
                 >
