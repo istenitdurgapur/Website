0c8392b7347e7cb2276622ed855a1ae8cd76ba20
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-06-24 11:01:09 +0530

    carousel created

---
+++ frontend/components/Carousel/Carousels.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import SwiperCore, { Virtual, Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
+
+// install Virtual module
+SwiperCore.use([ Navigation, Pagination]);
+
+export default function Carousels() {
+
+
+
+    return (
+        <>
+            <Swiper
+                slidesPerView={2}
+                spaceBetween={30}
+                centeredSlides={true}
+                loop={true}
+                loopFillGroupWithBlank={true}
+                pagination={{
+                  clickable: true,
+                }}
+                navigation={true}
+                modules={[Pagination, Navigation]}
+                className="mySwiper"
+                
+            >
+
+                <SwiperSlide>
+                    <img 
+                        src="/1.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>                  
+                    <img
+                    src="/2.jpeg" />             
+                     </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/3.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/1.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/2.jpeg" />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img
+                        src="/3.jpeg" />
+                </SwiperSlide>
+
+            </Swiper>
+
+
+        </>
+    );
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import s from "../styles/Home.module.scss";
 import Header from "../components/Header";
 import navigation from "../data";
 import Footer from "../components/Footer";
+import Carousels from '../components/Carousel/Carousels'
 
 export default function Home() {
   return (
@@ -24,12 +25,17 @@ export default function Home() {
           </p>
         </section>
         <section className={s.faculty}>
+          <h1 className="mb-6">PHOTO GALLERY</h1>
+        <Carousels/>
+        </section>
+        
+        <section className={s.faculty}>
           <h1>Faculty Advisors</h1>
           <div className={s.wrapper}>
             <div className={`${s.profileCard}`}>
               <div className={s.profileCard__img}>
                 <img
-                  src="https://res.cloudinary.com/muhammederdem/image/upload/v1537638518/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.jpg"
+                  src="http://istenitdgp.com/fa2.png"
                   alt="profile card"
                 />
               </div>
@@ -47,7 +53,7 @@ export default function Home() {
             <div className={`${s.profileCard}`}>
               <div className={s.profileCard__img}>
                 <img
-                  src="https://res.cloudinary.com/muhammederdem/image/upload/v1537638518/Ba%C5%9Fl%C4%B1ks%C4%B1z-1.jpg"
+                  src="http://istenitdgp.com/rowdra.png"
                   alt="profile card"
                 />
               </div>
@@ -65,43 +71,9 @@ export default function Home() {
               </div>
             </div>
           </div>
-          {/* <div className={s.wrapper}>
-            <div className={s.card}>
-              <img
-                src="https://images.pexels.com/photos/3785084/pexels-photo-3785084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                className={s.card__img}
-              />
-              <div className={s.card__body}>
-                <h2 className={s.card__title}>
-                  Dr. Anita Pal CHIEF-FACULTY ADVISOR
-                </h2>
-                <p className="card__description">
-                  Get abuzz and shroud yourself with knowledge to extinguish
-                  moribundity,as knowledge is a wing with which one can fly to
-                  the heaven.
-                </p>
-              </div>
-            </div>
-            <div className={s.card}>
-              <img
-                src="https://images.pexels.com/photos/3785084/pexels-photo-3785084.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
-                className={s.card__img}
-              />
-              <div className={s.card__body}>
-                <h2 className={s.card__title}>
-                  Prof. Rowdra Ghatak FACULTY ADVISOR
-                </h2>
-                <p className={s.card__description}>
-                  Nothing in this world can take the place of persistence.
-                  Talent will not: nothing is more common than unsuccessful men
-                  with talent. Genius will not; unrewarded genius is almost a
-                  proverb. Education will not: the world is full of educated
-                  derelicts. Persistence and determination alone are omnipotent.
-                </p>
-              </div>
-            </div>
-          </div> */}
+          
         </section>
+       
         <section className={s.sponsers}>
           <h1>Our sponsers</h1>
           <div className={s.slider}>
