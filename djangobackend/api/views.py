6717b645f0dde30e4695855796b19b6768ae3a10
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-07-17 23:33:13 +0530

    gmail sending implemented smtp

--- djangobackend/api/views.py
+++ djangobackend/api/views.py
@@ -1,6 +1,8 @@
 from .models import Alumni, Event, Student, gallery, sponsers
 from .serializers import AlumniSerializer, ContactSerializer, EventsSerializer, StudentSerializer, gallerySerializer, sponsersSerializer
 from rest_framework.generics import ListAPIView
+from django.core.mail import send_mail
+from django.conf import settings
 
 # Create your views here.
 class StudentList(ListAPIView):
@@ -36,5 +38,20 @@ def messages(request):
         contact_serializer = ContactSerializer(data=contact_data)
         if contact_serializer.is_valid():
             contact_serializer.save()
+            send_mail(
+				      "Message from"+	contact_data["name"] +" on ISTE Website",
+			        	contact_data["message"],
+			        	settings.EMAIL_HOST_USER,
+			        	['jnvomprakash121@gmail.com'],
+			        	fail_silently = False
+			        	)
+            send_mail(
+				      "Welcome to ISTE NIT Durgapur",
+			        	"we have got your message, Our team will reply you as soon as possible.😊",
+			        	settings.EMAIL_HOST_USER,
+			        	[contact_data["email"]],
+			        	fail_silently = False
+			        	)
             return JsonResponse(contact_serializer.data, status=status.HTTP_201_CREATED) 
         return JsonResponse(contact_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
--- djangobackend/djangobackend/settings.py
+++ djangobackend/djangobackend/settings.py
@@ -141,3 +141,9 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'opy19225090@gmail.com'
+EMAIL_HOST_PASSWORD = 'uqudfustitfeemkc'
+EMAIL_USE_TLS = True
