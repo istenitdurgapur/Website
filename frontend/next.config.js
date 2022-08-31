f95b53f88bcb7cf823cff3b2d482c8df156f0905
Abhishek Krishna <78419802+akrishna0402@users.noreply.github.com>
2022-08-31 22:39:17 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,7 +4,8 @@ const nextConfig = {
   images: {
     domains: [
       "127.0.0.1",
-      "https://istenitdgp.pythonanywhere.com"
+      "https://istenitdgp.pythonanywhere.com",
+      "https://backend.istenitdgp.com/"
     ],
   },
 }
