a2dbfd88d3277ebecc5c034adc28b6156e77567a
om-prakash-yadav <jnvomprakash121@gmail.com>
2022-09-02 01:00:39 +0530

    alumni updated

--- frontend/pages/alumni.js
+++ frontend/pages/alumni.js
@@ -1,14 +1,13 @@
 import React, { useEffect, useState } from "react";
-import Header from '../components/Header'
-import navigation from "../data"; 
-import Footer from '../components/Footer';
+import Header from "../components/Header";
+import navigation from "../data";
+import Footer from "../components/Footer";
 import AlumniSection from "../components/Alumni/AlumniSection";
 import Link from "next/link";
 import Loader from "../components/loader/Loader";
 import apiLinks from "../data/apiLink";
 
 function Alumni() {
-
   const [alumni, setAlumni] = useState([]);
 
   useEffect(() => {
@@ -20,80 +19,70 @@ function Alumni() {
   }, []);
 
   const styles = {
-    marginTop : "120px"
-  }
+    marginTop: "120px",
+  };
   const headerStyle = {
-    display : "flex",
-    width : "45%",
-    marginRight : "auto",
-    marginLeft : "auto",
-    alignItems : "center",
-    justifyContent : "space-between"
-  }
-  
-   // loader screen 
+    display: "flex",
+    width: "45%",
+    marginRight: "auto",
+    marginLeft: "auto",
+    alignItems: "center",
+    justifyContent: "space-between",
+  };
+
+  // loader screen
   //  const [spinner, setSpinner] = useState(true);
 
-   // It will be executed before rendering
- 
+  // It will be executed before rendering
+
   //  useEffect(() => {
   //    setTimeout(() => setSpinner(false), 0)
   //  }, []);
- 
+
   //  // [] means like componentDidMount
- 
- 
- 
+
   //  return spinner ? (
   //    <div>
   //    <Loader/>
   //    </div>
   //  ) :
- 
- 
+
   // (
-    return (
-    <div style={styles} >
+  return (
+    <div style={styles}>
       <Header items={navigation} navPosition="right" home={false} />
       <div className="flex justify-center align-center text-center ">
         <Link href="/team">
-          <h1 className=" text-3xl font-semibold flex text-center items-center justify-center" >TEAM</h1>
+          <h1 className=" text-3xl font-semibold flex text-center items-center justify-center">
+            TEAM
+          </h1>
         </Link>
         <h1 className="text-2xl flex items-center justify-center mx-6"> / </h1>
         <Link href="/alumni">
-        <h1 className="text-blue-600 text-3xl font-semibold flex text-center items-center justify-center underline decoration-1 decoration-black underline-offset-8" >ALUMNI</h1>
+          <h1 className="text-blue-600 text-3xl font-semibold flex text-center items-center justify-center underline decoration-1 decoration-black underline-offset-8">
+            ALUMNI
+          </h1>
         </Link>
       </div>
-      
-      <AlumniSection alumni={alumni}  year={2020} />
-      <AlumniSection alumni={alumni} year={2021} />
-      <AlumniSection alumni={alumni} year={2022} />
-      <AlumniSection alumni={alumni}  year={2023} />
-      <AlumniSection alumni={alumni} year={2024} />
-      <AlumniSection alumni={alumni} year={2025} />
-      <AlumniSection alumni={alumni}  year={2026} />
-      <AlumniSection alumni={alumni} year={2027} />
+
+      <AlumniSection alumni={alumni} year={2029} />
       <AlumniSection alumni={alumni} year={2028} />
-      <AlumniSection alumni={alumni}  year={2029} />
-      <AlumniSection alumni={alumni} year={2030} />
-      <AlumniSection alumni={alumni} year={2031} />
-      <AlumniSection alumni={alumni}  year={2032} />
-      <AlumniSection alumni={alumni} year={2033} />
-      <AlumniSection alumni={alumni} year={2034} />
-      <AlumniSection alumni={alumni}  year={2035} />
-      <AlumniSection alumni={alumni}  year={2036} />
-      <AlumniSection alumni={alumni}  year={2037} />
-      <AlumniSection alumni={alumni}  year={2038} />
-      <AlumniSection alumni={alumni}  year={2039} />
-      <AlumniSection alumni={alumni}  year={2040} />
-     
+      <AlumniSection alumni={alumni} year={2027} />
+      <AlumniSection alumni={alumni} year={2026} />
+      <AlumniSection alumni={alumni} year={2026} />
+      <AlumniSection alumni={alumni} year={2025} />
+      <AlumniSection alumni={alumni} year={2024} />
+      <AlumniSection alumni={alumni} year={2023} />
+      <AlumniSection alumni={alumni} year={2022} />
+      <AlumniSection alumni={alumni} year={2021} />
+      <AlumniSection alumni={alumni} year={2020} />
+
       <Footer />
     </div>
   );
 }
 
 export default Alumni;
-
 
 // export async function getStaticProps() {
 //   const res = await fetch("http://127.0.0.1:8000/api/alumni/");
